03e700da70856e05413f500f7e6ef547ed5fa98d
JaredSlayer12 <jaredborja62@gmail.com>
2025-03-12 13:29:00 +0800

    Attendance

---
+++ IPCU/Models/Trainee.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IPCU.Models
+{
+    public class Trainee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Sex { get; set; }
+
+        [Required]
+        public string Department { get; set; }
+
+        public string N95 { get; set; }
+
+        [Required]
+        public string IDPrinting { get; set; } // Dropdown (Done, Pending, Certificate)
+
+        public string BasicInfection { get; set; }
+
+        public string ProperHand { get; set; }
+
+        [Required]
+        public string GloveRemoval { get; set; } // Dropdown (PASSED, W/POWDER)
+
+        public int? PreTestScore { get; set; }
+
+        public int? PostTestScore { get; set; }
+
+        public decimal? Rate { get; set; }
+
+        public string TrainingReport { get; set; }
+    }
+}
