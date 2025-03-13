c3298cb1357a551e64df784d4eabe4311c0954b5
SuperOwe <clowiekianmanahan@gmail.com>
2025-03-13 11:20:13 +0800

    IPCUTEST

---
+++ IPCU/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IPCU.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult TakeTest()
+        {
+            return View();
+        }
+        public IActionResult Instructions()
+        {
+            return View(); // This will look for Views/Test/Instructions.cshtml
+        }
+
+    }
+}
